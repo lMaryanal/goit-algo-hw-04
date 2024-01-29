674eb3379fa3f145dfe6c655e07245c3fb33ec6e
lMaryanal <maryana.shcherbak@gmail.com>
2024-01-29 13:45:22 +0200

    final version

--- third_task.py
+++ third_task.py
@@ -2,46 +2,43 @@ import sys
 from pathlib import Path
 from colorama import Fore
 
-def catalogue(path):
+def catalogue(path, indent=0):
     if path.is_dir():
         name = path.name
-        print(f"{Fore.LIGHTYELLOW_EX}[directory]{Fore.RESET}: {name}")
+        indent = "|" + "  |"*(indent-1) + "--"*(bool(indent))
+        print(f"{indent}{Fore.LIGHTYELLOW_EX}[directory]{Fore.RESET}: {name}")
 
-def file(path):
+def file(path, indent=0):
     name = path.name
-    print(f"{Fore.LIGHTCYAN_EX}[file]{Fore.RESET}: {name}")
+    indent = "|" + "  |"*(indent-1)+"--"
+    print(f"{indent}{Fore.LIGHTCYAN_EX}[file]{Fore.RESET}: {name}")
 
-def open_directory(directory):
+def open_directory(directory, indent=0):
     for path in directory.iterdir():
         if path.is_dir():
-
-            catalogue(directory)
-            open_directory(path)
-        return()
-        if path.is_file():
-            file(path)
-        
-
-        
-
-        
-# Получаем аргументы командной строки
-arguments = sys.argv
+            catalogue(path, indent+1)
+            open_directory(path, indent+1)
+            
+    for path in directory.iterdir():
+         if path.is_file():
+            file(path, indent+1)
+    return
 
 
-# Проверяем, что передан хотя бы один аргумент (первый аргумент - имя самого скрипта)
-if len(arguments) < 2:
-    print("Использование: python script.py <параметр>")
-else:
-    path_to_the_directory = arguments[1]
-    directory = Path(path_to_the_directory)
-    if directory.exists() and directory.is_dir():
-       # print (catalogue(directory))
-        print(directory.iterdir())
-        #open_directory(directory)
+         
+def main():
+    arguments = sys.argv
+    if len(arguments) < 2:
+        print("Usage: python third_task.py <directory path>")
     else:
-        print("the catalogue does not exist")
+        path_to_the_directory = arguments[1]
+        directory = Path(path_to_the_directory)
+        if directory.exists() and directory.is_dir():
+            (catalogue(directory))
+            open_directory(directory)
+        else:
+            print("the catalogue does not exist")
     
+if __name__ == "__main__":
+    main()
 
-
-#python third_task.py F:\Repository\exemple
